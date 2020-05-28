c449b5943a5ab39116484f2887068c5d50e26d31
fage <fage@tg10010.com>
2020-05-28 09:52:56 +0800

    优化热更新插件生成后更新main.js添加热更新搜索路径代码

--- packages/hot-update-tools/main.js
+++ packages/hot-update-tools/main.js
@@ -10,16 +10,20 @@ module.exports = {
             Editor.log(e), Editor.Ipc.sendToPanel("hot-update-tools", "hot-update-tools:onBuildFinished")
         },
         "editor:build-finished": function (t, e) {
-            let o = require("fire-fs"),
-                a = require("fire-path");
+            let fs = require("fire-fs"),
+                path = require("fire-path");
             if (Editor.log("[HotUpdateTools] build platform:" + e.platform), "win32" === e.platform || "android" === e.platform || "ios" === e.platform || "mac" === e.platform) {
-                let t = a.normalize(e.dest),
-                    i = a.join(t, "main.js");
-                o.readFile(i, "utf8", function (t, e) {
-                    if (t) throw t;
-                    let a = e.replace("(function () {", "(function () { \n\n    if (cc && cc.sys.isNative) { \n        var hotUpdateSearchPaths = cc.sys.localStorage.getItem('HotUpdateSearchPaths'); \n        if (hotUpdateSearchPaths) { \n            jsb.fileUtils.setSearchPaths(JSON.parse(hotUpdateSearchPaths)); \n            console.log('[main.js] 热更新SearchPath: ' + JSON.parse(hotUpdateSearchPaths));\n        }else {\n            console.log('[main.js] 未获取到热更新资源路径!');\n        }\n    }else {\n        console.log('[main.js] 不是native平台!');\n    }\n");
-                    o.writeFile(i, a, function (e) {
-                        if (t) throw t;
+                let t = path.normalize(e.dest),
+                    i = path.join(t, "main.js");
+                fs.readFile(i, "utf8", function (error, content) {
+                    if (error) throw error;
+                    content = content.replace(/if\s*\(\s*window.jsb\)\s*\{/g,`if (window.jsb) {
+    var hotUpdateSearchPaths = localStorage.getItem('HotUpdateSearchPaths');
+    if (hotUpdateSearchPaths) {
+        jsb.fileUtils.setSearchPaths(JSON.parse(hotUpdateSearchPaths));
+    }`)
+                    fs.writeFile(i, content, function (e) {
+                        if (error) throw error;
                         Editor.log("[HotUpdateTools] SearchPath updated in built main.js for hot update")
                     })
                 })
