e1e68e89f482e9fcb632c909966a0e481d38edb6
zhengfasheng <547933762@qq.com>
2020-08-06 22:49:48 +0800

    去掉再面板中的显示，这个值目前没办法存下来

--- assets/script/framework/ui/UILabel.ts
+++ assets/script/framework/ui/UILabel.ts
@@ -1,10 +1,3 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 import { language } from "../base/Language";
 
 const {ccclass, property} = cc._decorator;
@@ -14,18 +7,15 @@ export default class UILabel extends cc.Label {
 
     private _lanKey:string = "language";
     set lanKey( value ) {
-        //cc.log(`set key : ${value}`);
         this._lanKey = value;
         this.string = language().get(value);
     }
-    @property({ displayName : "language key",tooltip:"语言包路径，如language,控制中String是由语言包生成的，如果使用这个控制，请使用语言包的key值"})
+    @property({ visible : false , displayName : "language key",tooltip:"语言包路径，如language,控制中String是由语言包生成的，如果使用这个控制，请使用语言包的key值"})
     get lanKey(){
-        //cc.log(`get key : ${this._lanKey}`);
         return this._lanKey;
     }
 
     onLoad(){
-        //cc.log(`onLoad key : ${this._lanKey}`);
         this.string = language().get(this._lanKey);
     }
     
