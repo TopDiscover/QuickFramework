6085689052158b7bcacb3e25b9dbfee51940b863
zheng_fasheng <547933762@qq.com>
2020-11-12 15:06:52 +0800

    2048

---
+++ assets/games/game2048/script/model/Game2048Item.ts
@@ -0,0 +1,9 @@
+
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Game2048Item extends cc.Component {
+
+   
+}
--- assets/games/game2048/script/view/Game2048GameView.ts
+++ assets/games/game2048/script/view/Game2048GameView.ts
@@ -1,5 +1,7 @@
 import GameView from "../../../../script/common/base/GameView";
 import { dispatchEnterComplete, LogicType } from "../../../../script/common/event/LogicEvent";
+import { Game2048 } from "../data/Game2048GameData";
+import Game2048Map from "../model/Game2048Map";
 
 const {ccclass, property} = cc._decorator;
 
@@ -10,9 +12,26 @@ export default class Game2048GameView extends GameView {
         return "prefabs/Game2048GameView"
     }
 
+    /**@description 分数 */
+    private score : cc.Label = null;
+
     onLoad(){
         super.onLoad();
 
+        Game2048.gameData.prefabs = cc.find("prefabs",this.node);
+
+        let gameMapNode = cc.find("gameMap",this.node);
+        let gameMap = gameMapNode.addComponent(Game2048Map);
+        gameMap.initMap();
+
+        this.score = cc.find("score",this.node).getComponent(cc.Label);
+
+        Game2048.gameData.socre = 100;
+
         dispatchEnterComplete( {type : LogicType.GAME,views:[this] });
+    }
+
+    public updateScore( score : number ){
+        this.score.string = score.toString();
     }
 }
