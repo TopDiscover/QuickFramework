1be322652bc03c481e7d5de100559c1a345a1e16
zhengfasheng <547933762@qq.com>
2020-11-23 21:48:49 +0800

    1

--- assets/bundles/hall/script/controller/HallNetHelper.ts
+++ assets/bundles/hall/script/controller/HallNetHelper.ts
@@ -5,21 +5,35 @@ import { TestBinaryMessage } from "../protocol/CmdBinaryMessage";
 import { HttpPackage, HttpRequestType } from "../../../../script/framework/net/HttpClient";
 import { GameService } from "../../../../script/common/net/GameService";
 
-
-class _HallNetHelper extends NetHelper<LobbyService>{
-
-    constructor(){
-        super(LobbyService.instance);
+class _GameHelper extends NetHelper<LobbyService>{
+    constructor() {
+        super(GameService.instance);
     }
 
-    private gameService : GameService  = GameService.instance;
 
     sendProtoMessage() {
         let testProto = new TestMsg();
         testProto.data.awesomeField = "这是一个中文的测试";
         testProto.data.afvalue = 4.5;
         this.service.send(testProto);
-        this.gameService.send(testProto);
+    }
+}
+
+class _HallNetHelper extends NetHelper<LobbyService>{
+
+    
+    private gameHelper = new _GameHelper();
+
+    constructor() {
+        super(LobbyService.instance);
+    }
+
+    sendProtoMessage() {
+        let testProto = new TestMsg();
+        testProto.data.awesomeField = "这是一个中文的测试";
+        testProto.data.afvalue = 4.5;
+        this.service.send(testProto);
+        this.gameHelper.sendProtoMessage();
     }
 
     sendJsonMessage() {
@@ -32,18 +46,18 @@ class _HallNetHelper extends NetHelper<LobbyService>{
         this.service.send(binaryMessage);
     }
 
-    sendHttpMessage( ){
+    sendHttpMessage() {
 
         let httpPackage = new HttpPackage();
         httpPackage.data.url = "https://httpbin.org/post";
         httpPackage.data.type = HttpRequestType.POST;
         //"text/plain;charset=UTF-8""Accept-Encoding","gzip,deflate"
         //httpPackage.data.requestHeader = [{name : "Content-Type" , value : "text/plain"},{name:"Accept-Encoding",value:"gzip,deflate"}]
-        httpPackage.data.requestHeader = {name : "Content-Type" , value : "text/plain"}
-        httpPackage.data.data = new Uint8Array([1,2,3,4,5]);
-        httpPackage.send((data)=>{
+        httpPackage.data.requestHeader = { name: "Content-Type", value: "text/plain" }
+        httpPackage.data.data = new Uint8Array([1, 2, 3, 4, 5]);
+        httpPackage.send((data) => {
             cc.log("数据返回")
-        },()=>{
+        }, () => {
             cc.log("数据错误")
         })
 
@@ -51,12 +65,12 @@ class _HallNetHelper extends NetHelper<LobbyService>{
         httpPackage.data.url = "https://httpbin.org/get";
         // httpPackage.data.requestHeader = {name : "Content-Type" , value : "text/plain"}
         httpPackage.params = {
-            a : 100,
-            b : "zheng"
+            a: 100,
+            b: "zheng"
         }
-        httpPackage.send((data)=>{
+        httpPackage.send((data) => {
             cc.log("数据返回")
-        },()=>{
+        }, () => {
             cc.log("数据错误")
         })
 
