c439ee8ca593e59cf5f12cd3dc18035e3a104cc8
zhengfasheng <547933762@qq.com>
2021-06-02 23:33:35 +0800

    添加代理

---
+++ assets/scripts/framework/ui/TipsDelegate.ts
@@ -0,0 +1,22 @@
+import { Node } from "cc"
+
+ /**@description 提示代理 */
+ export default class TipsDelegate {
+
+    /**
+     * @description tips提示
+     * @param msg 提示内容
+     */
+    public show( msg : string ) {
+        
+    }
+
+    /**@description 预加载预置体 */
+    public preloadPrefab(){
+
+    }
+
+    public finishShowItem( node : Node ){
+        
+    }
+}
