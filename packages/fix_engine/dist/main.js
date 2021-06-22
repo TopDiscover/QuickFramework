d50168264d71f35c49104c7a22a8e8984c86a010
zhengfasheng <547933762@qq.com>
2021-06-22 20:26:26 +0800

    插件更新

--- packages/fix_engine/dist/main.js
+++ packages/fix_engine/dist/main.js
@@ -1,27 +1,8 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.messages = exports.unload = exports.load = exports._Helper = void 0;
-const fs = __importStar(require("fs"));
-const path = __importStar(require("path"));
+const fs = require("fs");
+const path = require("path");
 class _Helper {
     constructor() {
         /**@description creator 安所路径 */
@@ -107,7 +88,7 @@ class _Helper {
     }
     /**@description 对哪些creator版本有效 */
     get validVersions() {
-        return ["2.4.0", "2.4.1", "2.4.2", "2.4.3"];
+        return ["2.4.0", "2.4.1", "2.4.2", "2.4.3", "2.4.4"];
     }
     run() {
         Editor.log(`Creator Version : ${this.creatorPluginVersion}`);
