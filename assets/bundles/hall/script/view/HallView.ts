969e2a133592612e23c89a3b1b474eb7bff7e75f
zhengfasheng <547933762@qq.com>
2021-08-20 22:57:11 +0800

    修正下载无进度显示问题

--- assets/bundles/hall/script/view/HallView.ts
+++ assets/bundles/hall/script/view/HallView.ts
@@ -21,10 +21,11 @@ export default class HallView extends UIView {
         Manager.bundleManager.enterBundle(this.bundles[ev.target.userData]);
     }
 
-    private gamePage : cc.Node = null;
-    private gameItem : cc.Node = null;
-    private pageView : cc.PageView = null;
+    private gamePage: cc.Node = null;
+    private gameItem: cc.Node = null;
+    private pageView: cc.PageView = null;
     private readonly PAGE_COUNT = 6;
+    private testProgress = 0;
 
     private _bundles: BundleConfig[] = [];
     private get bundles() {
@@ -44,11 +45,11 @@ export default class HallView extends UIView {
         return this._bundles;
     }
 
-    private createPage(){
+    private createPage() {
 
         //计算出总页数
-        let pageCount = Math.ceil( this.bundles.length / this.PAGE_COUNT );
-        for( let i = 0 ;i < pageCount ; i++ ){
+        let pageCount = Math.ceil(this.bundles.length / this.PAGE_COUNT);
+        for (let i = 0; i < pageCount; i++) {
             let page = cc.instantiate(this.gamePage);
             page.active = true;
             this.pageView.addPage(page);
@@ -63,7 +64,7 @@ export default class HallView extends UIView {
             game.on(cc.Node.EventType.TOUCH_END, this.onClick, this);
 
             //计算出所有页
-            let page = Math.floor(i/this.PAGE_COUNT);
+            let page = Math.floor(i / this.PAGE_COUNT);
             this.pageView.getPages()[page].addChild(game);
         }
     }
@@ -72,21 +73,22 @@ export default class HallView extends UIView {
         super.onLoad();
         this.gamePage = cc.find("games", this.node);
         this.gameItem = cc.find("gameItem", this.node);
-        this.pageView = cc.find("pageview",this.node).getComponent(cc.PageView);
+        this.pageView = cc.find("pageview", this.node).getComponent(cc.PageView);
         this.createPage();
 
         let bottom_op = cc.find("bottom_op", this.node);
         let setting = cc.find("setting", bottom_op);
         setting.on(cc.Node.EventType.TOUCH_END, () => {
-            Manager.uiManager.open({type:SettingView,bundle:td.Macro.BUNDLE_RESOURCES,zIndex:td.ViewZOrder.UI,name:"设置界面"});
+            Manager.uiManager.open({ type: SettingView, bundle: td.Macro.BUNDLE_RESOURCES, zIndex: td.ViewZOrder.UI, name: "设置界面" });
         });
 
         LobbyService.instance.enabled = false;
         GameService.instance.enabled = false;
         ChatService.instance.enabled = false;
 
-        let mail = cc.find("mial",bottom_op);
-        mail.on(cc.Node.EventType.TOUCH_END,()=>{
+
+        let mail = cc.find("mial", bottom_op);
+        mail.on(cc.Node.EventType.TOUCH_END, () => {
             let lan = Manager.language.getLanguage();
             if( lan == cc.sys.LANGUAGE_CHINESE){
                 lan = cc.sys.LANGUAGE_ENGLISH
@@ -94,10 +96,15 @@ export default class HallView extends UIView {
                 lan = cc.sys.LANGUAGE_CHINESE;
             }
             Manager.language.change(lan);
+            // dispatch(CommonEvent.DOWNLOAD_PROGRESS, { progress: this.testProgress, config: this.bundles[0] });
+            // this.testProgress += 0.1;
+            // if (this.testProgress >= 1) {
+            //     this.testProgress = 0;
+            // }
         });
 
         // this.audioHelper.playMusic("audio/background",this.bundle)
-        
+
         dispatchEnterComplete({ type: LogicType.HALL, views: [this] });
     }
 
@@ -106,18 +113,33 @@ export default class HallView extends UIView {
         this.registerEvent(CommonEvent.DOWNLOAD_PROGRESS, this.onDownloadProgess);
     }
 
+    private getGameItem(config: BundleConfig) {
+        let pages = this.pageView.getPages();
+        for (let i = 0; i < pages.length; i++) {
+            let page = pages[i];
+            let item = cc.find(`game_${config.index}`, page);
+            if (item) {
+                return item;
+            }
+        }
+        return null;
+    }
+
     private onDownloadProgess(data: { progress: number, config: BundleConfig }) {
 
-        let progressBar: cc.ProgressBar = cc.find(`games/game_${data.config.index}/Background/progressBar`, this.node).getComponent(cc.ProgressBar);
-        let progressLabel: cc.Label = cc.find(`games/game_${data.config.index}/Background/progressBar/progress`, this.node).getComponent(cc.Label);
-        if (data.progress == -1) {
-            progressBar.node.active = false;
-        } else if (data.progress < 1) {
-            progressBar.node.active = true;
-            progressBar.progress = data.progress;
-            progressLabel.string = "" + Math.floor(data.progress * 100) + "%";
-        } else {
-            progressBar.node.active = false;
+        let node = this.getGameItem(data.config);
+        if (node) {
+            let progressBar: cc.ProgressBar = cc.find(`Background/progressBar`, node).getComponent(cc.ProgressBar);
+            let progressLabel: cc.Label = cc.find(`Background/progressBar/progress`, node).getComponent(cc.Label);
+            if (data.progress == -1) {
+                progressBar.node.active = false;
+            } else if (data.progress < 1) {
+                progressBar.node.active = true;
+                progressBar.progress = data.progress;
+                progressLabel.string = "" + Math.floor(data.progress * 100) + "%";
+            } else {
+                progressBar.node.active = false;
+            }
         }
     }
 }
