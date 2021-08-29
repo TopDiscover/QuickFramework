c5e398d3efc2127ba7e689a7ff4b8a62559c3129
zhengfasheng <zheng_fasheng@outlook.com>
2021-08-29 22:26:27 +0800

    11

--- assets/scripts/login/view/LoginView.ts
+++ assets/scripts/login/view/LoginView.ts
@@ -1,7 +1,5 @@
 import { UIView } from "../../framework/support/ui/UIView";
 import DownloadLoading from "../../common/component/DownloadLoading";
-import { NetManager } from "../../framework/support/net/service/NetManager";
-import { AssetManager } from "../../framework/support/assetManager/AssetManager";
 
 const { ccclass, property } = cc._decorator;
 
@@ -18,15 +16,7 @@ export default class LoginView extends UIView {
         super.onLoad();
         this._login = cc.find("login", this.node);
         this._login.on(cc.Node.EventType.TOUCH_END, () => {
-            // console.log()
-            console.log(cc.js.getClassName(this))
-
-            //@ts-ignore
-            console.log(cc.js.getClassName(NetManager))
-            console.log(cc.js.getClassName(new NetManager("123")))
-
-            console.log(this.getClassName(AssetManager))
-            // Manager.bundleManager.enterBundle(new td.HotUpdate.BundleConfig("大厅", td.Config.BUNDLE_HALL, 0, td.Logic.Event.ENTER_HALL, true));
+            Manager.bundleManager.enterBundle(new td.HotUpdate.BundleConfig("大厅", td.Config.BUNDLE_HALL, 0, td.Logic.Event.ENTER_HALL, true));
         });
 
         // let version = cc.find("version", this.node).getComponent(cc.Label).string = "版本3";
