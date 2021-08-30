b6e265577f3f49270d7da73a5acb8d570333991c
zhengfasheng <zheng_fasheng@outlook.com>
2021-08-30 13:44:14 +0800

    冲突代码合并

--- assets/scripts/framework/decorator/Decorators.ts
+++ assets/scripts/framework/decorator/Decorators.ts
@@ -2,25 +2,61 @@
 /**
  * @description 各种装饰器定义
  */
-import { Service } from "../core/net/service/Service";
 
-export function injectService(service: Service) {
+import { Codec } from "../core/net/message/Message"
+import { Service } from "../core/net/service/Service"
+
+export function setServiceByClassName(name: string) {
+    // if(CC_DEBUG)    
     return function (target: any) {
-        let __load = target.prototype.onLoad;
+        let __load = target.prototype.onLoad
         target.prototype.onLoad = function () {
-            if ( CC_DEBUG ) cc.log(`[injectService] ${cc.js.getClassName(this)} ---onLoad----`);
-            this.service = service;
-            __load && __load.call(this);
+            if (CC_DEBUG) {
+                cc.log(`[setService] ${cc.js.getClassName(this)} onLoad`)
+            }
+            let service = Manager.serviceManager.getServiceByNmame(name)
+            if (CC_DEBUG && !service) {
+                cc.log(`[ByNameSetService] 在 ${cc.js.getClassName(this)} 注入[${name}]失败! `)
+                service = null
+            }
+            this._service = service
+            __load && __load.call(this)
+        }
+    }
+}
+export function setService(service: Service) {
+    return function (target: any) {
+        let __load = target.prototype.onLoad
+        target.prototype.onLoad = function () {
+            if (CC_DEBUG) {
+                cc.log(`[setService] ${cc.js.getClassName(this)} onLoad`)
+            }
+            this._service = service
+            __load && __load.call(this)
         }
     }
 }
 
-/**
-* @description 生成key 如果需要改变请 连带decorators 中的protoHandle 方法一起改动，这两个地方使用的同一个生成规则
-* @param mainCmd 
-* @param subCmd 
-*/
-export function makeKey(mainCmd: number, subCmd: number): string {
-    let key = `[${mainCmd}]:[${subCmd}]`;
-    return key;
+export function setServiceCodec(header: typeof Codec) {
+    return function (target: any) {
+        let __load = target.prototype.onLoad
+        target.prototype.onLoad = function () {
+            if (CC_DEBUG) {
+                cc.log(`[setServiceCodec] ${cc.js.getClassName(this)} onLoad`)
+            }
+            if (this._service) {
+                this._service.Codec = header
+            }
+
+            __load && __load.call(this)
+        }
+    }
+}
+
+export function setClassName() {
+    return function (target) {
+        let frameInfo = cc['_RF'].peek()
+        let script = frameInfo.script;
+        cc.js.setClassName(script, target)
+    }
 }
