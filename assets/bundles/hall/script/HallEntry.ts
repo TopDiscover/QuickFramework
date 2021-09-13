150a6ce0e00bb0aea38041fe785f2c6fa78abe1a
zhengfasheng <547933762@qq.com>
2021-09-14 01:05:03 +0800

    合并

--- assets/bundles/hall/script/HallEntry.ts
+++ assets/bundles/hall/script/HallEntry.ts
@@ -11,7 +11,7 @@ class HallEntry extends Entry {
         Manager.hallNetManager.addNetControllers();
     }
     protected removeNetComponent(): void {
-        Manager.hallNetManager.removeNetControllers();
+        // Manager.hallNetManager.removeNetControllers();
     }
     protected loadResources(completeCb: () => void): void {
         Manager.protoManager.load(this.bundle).then((isSuccess)=>{
