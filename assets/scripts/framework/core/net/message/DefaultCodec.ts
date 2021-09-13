df96fb7749da271e6f8c6b36ca955be3fb867385
zhengfasheng <547933762@qq.com>
2021-09-14 00:51:11 +0800

    合并

---
+++ assets/scripts/framework/core/net/message/DefaultCodec.ts
@@ -0,0 +1,70 @@
+import { Macro } from "../../../defines/Macros";
+import { Codec } from "./Message";
+
+export interface MessageStruct {
+    mainCmd: number
+    subCmd: number
+    buffer: Uint8Array
+}
+export class DefaultCodec extends Codec {
+    /**@description 消息主cmd码 */
+    mainCmd: number = 0;
+    /**@description 消息子cmd码 */
+    subCmd: number = 0;
+    /**@description 数据buffer */
+    buffer: Uint8Array = null!;
+    /**@description 数据大小 */
+    private _dataSize: number = 0;
+    headerSize: number = 3 * Uint32Array.BYTES_PER_ELEMENT;
+
+    pack(data: MessageStruct): boolean {
+        this.mainCmd = data.mainCmd;
+        this.subCmd = data.subCmd;
+        this._dataSize = 0;
+        let offset = 0;
+
+        /**第一种写法 */
+        if (data.buffer) {
+            //如果有包体，先放入包体
+            this._dataSize = data.buffer.length;
+        }
+
+        let buffer = new ArrayBuffer(this._dataSize + this.headerSize);
+        let dataView = new DataView(buffer);
+        dataView.setUint32(offset, this.mainCmd, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        dataView.setUint32(offset, this.subCmd, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        dataView.setUint32(offset, this._dataSize, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        if (data.buffer) {
+            //感觉这里的复制数据有点low啊
+            for (let i = 0; i < data.buffer.byteLength; i++) {
+                dataView.setUint8(offset, data.buffer[i]);
+                offset += Uint8Array.BYTES_PER_ELEMENT;
+            }
+        }
+        let result = new Uint8Array(dataView.buffer);
+
+        this.buffer = result;
+        return true;
+    }
+    unPack(data: any): boolean {
+        let dataView = new DataView(data.buffer);
+        //取包头
+        let offset = 0;
+        this.mainCmd = dataView.getUint32(offset, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        this.subCmd = dataView.getUint32(offset, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        this._dataSize = dataView.getUint32(offset, Macro.USING_LITTLE_ENDIAN);
+        offset += Uint32Array.BYTES_PER_ELEMENT;
+        let buffer = dataView.buffer.slice(offset, dataView.buffer.byteLength)
+        this.buffer = new Uint8Array(buffer);
+        return this._dataSize == this.buffer.length;
+    }
+
+
+    get cmd(){return String(this.mainCmd) + String(this.subCmd);} 
+
+}
