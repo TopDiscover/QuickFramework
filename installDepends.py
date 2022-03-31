61f63336138ac37e587e7223616836c7b627293a
zhengfasheng <547933762@qq.com>
2022-03-31 21:13:54 +0800

    框架结构修改

--- installDepends.py
+++ installDepends.py
@@ -14,10 +14,9 @@ class Install :
         self.curPath = os.getcwd()
         print(u"当前位置:".encode("gbk") + self.curPath)
         # 当前项目路径
-        self.projPath = os.getcwd()
+        self.projPath = os.path.join(self.curPath,"proj")
         print(u"当前工程位置:".encode("gbk") + self.projPath)
         # 需要安装目录记录
-        # self.extensions = ["fix_engine","gulp-compress","hotupdate","png-compress"]
         self.extensions = ["fix_engine","hotupdate","png-compress","test-server"]
 
     # 执行命令
@@ -39,11 +38,7 @@ class Install :
 
         cmd = "npm install"
 
-        #安装外层依赖库
-        print(u"安装项目依赖库".encode("gbk"))
-        self.runCommand(cmd,True)
-
-        #安装扩展插件依赖 //["fix_engine","hotupdate","png-compress"]
+        #安装扩展插件依赖
         for v in self.extensions :
             temp = "extensions/" + v
             fullpath = os.path.join(self.projPath,temp)
@@ -53,7 +48,7 @@ class Install :
             self.runCommand(cmd,True)
 
         # 安装tsrpc
-        fullpath = os.path.join(self.projPath,"tsrpc")
+        fullpath = os.path.join(self.curPath,"tools/tsrpc")
         print(u"安装依赖库:".encode("gbk") + "tsrpc")
         os.chdir(fullpath)
         print(u"当前目录:" + os.getcwd())
