0187ff0d2e8173e01252777279f142108b2e2fef
zhengfasheng <547933762@qq.com>
2022-03-31 21:11:26 +0800

    框架结构修改

--- tools/tsrpc/tsrpc.config.ts
+++ tools/tsrpc/tsrpc.config.ts
@@ -4,8 +4,8 @@ const tsrpcConf: TsrpcConfig = {
     // Sync shared code , 同步连接bundles
     sync: [
         {
-            from: '../Bundles/bundles',
-            to: '../assets/bundles',
+            from: '../../bundles/bundles',
+            to: '../../proj/assets/bundles',
             type: 'symlink'     // Change this to 'copy' if your environment not support symlink
         }
     ],
