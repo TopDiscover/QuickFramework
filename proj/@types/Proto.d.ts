0f296a6f4a018f795286c4d7fefd3f1d8960755a
zhengfasheng <zheng_fasheng@outlook.com>
2023-04-01 18:53:24 +0800

    升级适配

--- proj/@types/Proto.d.ts
+++ proj/@types/Proto.d.ts
@@ -3,6 +3,115 @@
 /** Namespace tp. */
 declare namespace tp {
 
+    /** Properties of a RoomInfo. */
+    interface IRoomInfo {
+
+        /** RoomInfo roomID */
+        roomID?: (number|null);
+
+        /** RoomInfo name */
+        name?: (string|null);
+
+        /** RoomInfo players */
+        players?: (tp.IUserInfo[]|null);
+    }
+
+    /** Represents a RoomInfo. */
+    class RoomInfo implements IRoomInfo {
+
+        /**
+         * Constructs a new RoomInfo.
+         * @param [properties] Properties to set
+         */
+        constructor(properties?: tp.IRoomInfo);
+
+        /** RoomInfo roomID. */
+        public roomID: number;
+
+        /** RoomInfo name. */
+        public name: string;
+
+        /** RoomInfo players. */
+        public players: tp.IUserInfo[];
+
+        /**
+         * Creates a new RoomInfo instance using the specified properties.
+         * @param [properties] Properties to set
+         * @returns RoomInfo instance
+         */
+        public static create(properties?: tp.IRoomInfo): tp.RoomInfo;
+
+        /**
+         * Encodes the specified RoomInfo message. Does not implicitly {@link tp.RoomInfo.verify|verify} messages.
+         * @param message RoomInfo message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encode(message: tp.IRoomInfo, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Encodes the specified RoomInfo message, length delimited. Does not implicitly {@link tp.RoomInfo.verify|verify} messages.
+         * @param message RoomInfo message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encodeDelimited(message: tp.IRoomInfo, writer?: $protobuf.Writer): $protobuf.Writer;
+
+        /**
+         * Decodes a RoomInfo message from the specified reader or buffer.
+         * @param reader Reader or buffer to decode from
+         * @param [length] Message length if known beforehand
+         * @returns RoomInfo
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): tp.RoomInfo;
+
+        /**
+         * Decodes a RoomInfo message from the specified reader or buffer, length delimited.
+         * @param reader Reader or buffer to decode from
+         * @returns RoomInfo
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {$protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): tp.RoomInfo;
+
+        /**
+         * Verifies a RoomInfo message.
+         * @param message Plain object to verify
+         * @returns `null` if valid, otherwise the reason why it is not
+         */
+        public static verify(message: { [k: string]: any }): (string|null);
+
+        /**
+         * Creates a RoomInfo message from a plain object. Also converts values to their respective internal types.
+         * @param object Plain object
+         * @returns RoomInfo
+         */
+        public static fromObject(object: { [k: string]: any }): tp.RoomInfo;
+
+        /**
+         * Creates a plain object from a RoomInfo message. Also converts values to other types if specified.
+         * @param message RoomInfo
+         * @param [options] Conversion options
+         * @returns Plain object
+         */
+        public static toObject(message: tp.RoomInfo, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+        /**
+         * Converts this RoomInfo to JSON.
+         * @returns JSON object
+         */
+        public toJSON(): { [k: string]: any };
+
+        /**
+         * Gets the default type url for RoomInfo
+         * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+         * @returns The default type url
+         */
+        public static getTypeUrl(typeUrlPrefix?: string): string;
+    }
+
     /** GenderType enum. */
     enum GenderType {
         male = 1,
@@ -124,115 +233,6 @@ declare namespace tp {
 
         /**
          * Gets the default type url for UserInfo
-         * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
-         * @returns The default type url
-         */
-        public static getTypeUrl(typeUrlPrefix?: string): string;
-    }
-
-    /** Properties of a RoomInfo. */
-    interface IRoomInfo {
-
-        /** RoomInfo roomID */
-        roomID?: (number|null);
-
-        /** RoomInfo name */
-        name?: (string|null);
-
-        /** RoomInfo players */
-        players?: (tp.IUserInfo[]|null);
-    }
-
-    /** Represents a RoomInfo. */
-    class RoomInfo implements IRoomInfo {
-
-        /**
-         * Constructs a new RoomInfo.
-         * @param [properties] Properties to set
-         */
-        constructor(properties?: tp.IRoomInfo);
-
-        /** RoomInfo roomID. */
-        public roomID: number;
-
-        /** RoomInfo name. */
-        public name: string;
-
-        /** RoomInfo players. */
-        public players: tp.IUserInfo[];
-
-        /**
-         * Creates a new RoomInfo instance using the specified properties.
-         * @param [properties] Properties to set
-         * @returns RoomInfo instance
-         */
-        public static create(properties?: tp.IRoomInfo): tp.RoomInfo;
-
-        /**
-         * Encodes the specified RoomInfo message. Does not implicitly {@link tp.RoomInfo.verify|verify} messages.
-         * @param message RoomInfo message or plain object to encode
-         * @param [writer] Writer to encode to
-         * @returns Writer
-         */
-        public static encode(message: tp.IRoomInfo, writer?: $protobuf.Writer): $protobuf.Writer;
-
-        /**
-         * Encodes the specified RoomInfo message, length delimited. Does not implicitly {@link tp.RoomInfo.verify|verify} messages.
-         * @param message RoomInfo message or plain object to encode
-         * @param [writer] Writer to encode to
-         * @returns Writer
-         */
-        public static encodeDelimited(message: tp.IRoomInfo, writer?: $protobuf.Writer): $protobuf.Writer;
-
-        /**
-         * Decodes a RoomInfo message from the specified reader or buffer.
-         * @param reader Reader or buffer to decode from
-         * @param [length] Message length if known beforehand
-         * @returns RoomInfo
-         * @throws {Error} If the payload is not a reader or valid buffer
-         * @throws {$protobuf.util.ProtocolError} If required fields are missing
-         */
-        public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): tp.RoomInfo;
-
-        /**
-         * Decodes a RoomInfo message from the specified reader or buffer, length delimited.
-         * @param reader Reader or buffer to decode from
-         * @returns RoomInfo
-         * @throws {Error} If the payload is not a reader or valid buffer
-         * @throws {$protobuf.util.ProtocolError} If required fields are missing
-         */
-        public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): tp.RoomInfo;
-
-        /**
-         * Verifies a RoomInfo message.
-         * @param message Plain object to verify
-         * @returns `null` if valid, otherwise the reason why it is not
-         */
-        public static verify(message: { [k: string]: any }): (string|null);
-
-        /**
-         * Creates a RoomInfo message from a plain object. Also converts values to their respective internal types.
-         * @param object Plain object
-         * @returns RoomInfo
-         */
-        public static fromObject(object: { [k: string]: any }): tp.RoomInfo;
-
-        /**
-         * Creates a plain object from a RoomInfo message. Also converts values to other types if specified.
-         * @param message RoomInfo
-         * @param [options] Conversion options
-         * @returns Plain object
-         */
-        public static toObject(message: tp.RoomInfo, options?: $protobuf.IConversionOptions): { [k: string]: any };
-
-        /**
-         * Converts this RoomInfo to JSON.
-         * @returns JSON object
-         */
-        public toJSON(): { [k: string]: any };
-
-        /**
-         * Gets the default type url for RoomInfo
          * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
          * @returns The default type url
          */
