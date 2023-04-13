be439ea65f460959fb5b240fbd4207553e9b727b
zhengfasheng <zheng_fasheng@outlook.com>
2023-04-13 15:25:50 +0800

    inject 接口优化,再首次使用时，才进行节点查找

--- proj/assets/scripts/framework/defines/Decorators.ts
+++ proj/assets/scripts/framework/defines/Decorators.ts
@@ -3,15 +3,15 @@
  */
 
 import GameView from "../core/ui/GameView";
+const _FIND_OPTIONS_ = "_FIND_OPTIONS_"
 
-const finddataKey = "[[Finddata]]"
-const findOptions = "[[Findoption]]"
+interface FIND_TYPE<T> {
+	new(): T;
+}
 
-type FindOptionType = typeof cc.Component | typeof cc.Node;
-
-interface FindOption {
+interface FindOption<T> {
     path: string;
-    type: FindOptionType;
+    type: FIND_TYPE<T>;
     member: string;
 }
 
@@ -30,68 +30,60 @@ export function registerEntry(className: string, bundle: string, type: typeof Ga
     }
 }
 
+
+function __find<T>( path : string , node : cc.Node , type : FIND_TYPE<T> ){
+    let temp = cc.find(path,node);
+    if ( cc.js.isChildClassOf(type,cc.Component) ){
+        let comp = temp?.getComponent(type)
+        return comp;
+    }
+    return temp;
+}
+
 /**
  * @description 当onLoad时，按指定节点搜索路径，注入节点或组件到指定成员变量
  * @param path 相对于当前脚本this.node的搜索路径
  * @param type 查找组件类型
  * @returns 
  */
-export function inject(path: string, type: FindOptionType) {
+export function inject<T extends cc.Component | cc.Node>(path: string, type: FIND_TYPE<T>) {
     return function (target: any, member: string) {
         if (!(target instanceof cc.Component)) {
             Log.e("无法注入,仅支持 Component 组件")
             return;
         }
         let obj: any = target;
-        if (!Reflect.has(target, findOptions)) {
-            let funName = "onLoad"
-            let aHookFun = obj[funName];
-            obj[funName] = function () {
-                // Log.d(this, this.node.name)
-                if (!Reflect.has(this, finddataKey)) {
-                    Reflect.defineProperty(this, finddataKey, { value: {} })
-                }
-                let self: any = this
-                let fOption = Reflect.get(self, findOptions)
+        if (!Reflect.has(target, _FIND_OPTIONS_)) {
+            let __onLoad = obj.onLoad
+            obj.onLoad = function () {
+                let self = this;
+                let fOption = Reflect.get(self, _FIND_OPTIONS_)
                 for (let key in fOption) {
-                    let ele : FindOption = Reflect.get(fOption, key)
-                    self.__defineGetter__(ele.member, function () {
-                        if (!Reflect.has(self[finddataKey], key)) {
-                            let node = cc.find(ele.path,self.node);
-                            if ( cc.js.isChildClassOf(ele.type,cc.Component)){
-                                let comp = node?.getComponent(ele.type as any);
-                                if ( Reflect.has(self[finddataKey],key) ){
-                                    self[finddataKey][key] = comp;
-                                }else{
-                                    Reflect.defineProperty(self[finddataKey],key,{ value : comp , writable : true});
+                    let ele : FindOption<T> = Reflect.get(fOption, key)
+                    if ( !Reflect.get(self,ele.member) ){
+                        Reflect.defineProperty(self,ele.member,{
+                            enumerable : true,
+                            configurable : true,
+                            get(){
+                                if ( !cc.isValid(self[key]) ){
+                                    self[key] = __find(ele.path,self.node,ele.type);
                                 }
-                            }else{
-                                if ( Reflect.has(self[finddataKey],key) ){
-                                    self[finddataKey][key] = node;
-                                }else{
-                                    Reflect.defineProperty(self[finddataKey],key,{ value : node , writable : true});
-                                }
+                                return self[key];
+                            },
+                            set(v){
+                                self[key] = v;
                             }
-                        }
-                        return self[finddataKey][key]
-                    })
-
-                    self.__defineSetter__(ele.member, function (val: any) {
-                        if (Reflect.has(self[finddataKey], key)) {
-                            self[finddataKey][key] = val
-                        } else {
-                            Reflect.defineProperty(self[finddataKey], key, { value: val, writable: true })
-                        }
-                    })
+                        })
+                    }
                 }
-                aHookFun && Reflect.apply(aHookFun, this, arguments);
+                __onLoad && Reflect.apply(__onLoad, this, arguments);
             }
-            Reflect.defineProperty(target, findOptions, { value: {} })
+            Reflect.defineProperty(target, _FIND_OPTIONS_, { value: {} })
         }
 
-        let option: FindOption = { path: path, type: type, member: member }
-        let attribute = "__" + member + "__"
-        let fOption = Reflect.get(target, findOptions)
+        let option: FindOption<T> = { path: path, type: type, member: member }
+        let attribute = `__${member}`
+        let fOption = Reflect.get(target, _FIND_OPTIONS_)
         Reflect.defineProperty(fOption, attribute, { value: option, enumerable: true })
     }
 }
