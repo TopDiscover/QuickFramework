c07576429797e2ae3af8ff4f97b81ef5244574a7
zhengfasheng <zheng_fasheng@outlook.com>
2023-04-13 17:19:47 +0800

    inject 接口优化,再首次使用时，才进行节点查找

--- proj/assets/scripts/framework/defines/Decorators.ts
+++ proj/assets/scripts/framework/defines/Decorators.ts
@@ -3,6 +3,7 @@
  */
 
 import { Component, find, isValid, js, Node } from "cc";
+import { DEBUG } from "cc/env";
 import GameView from "../core/ui/GameView";
 const _FIND_OPTIONS_ = "_FIND_OPTIONS_"
 
@@ -14,6 +15,7 @@ interface FindOption<T> {
     path: string;
     type: FIND_TYPE<T>;
     member: string;
+    root?: string;
 }
 
 /**
@@ -42,12 +44,13 @@ function __find<T>(path: string, node: Node, type: FIND_TYPE<T>) {
 }
 
 /**
- * @description 当onLoad时，按指定节点搜索路径，注入节点或组件到指定成员变量
- * @param path 相对于当前脚本this.node的搜索路径
+ * @description 当onLoad时，自动对所有注入的成员变量设置set&get方法,当成员变量首次调用时对成员变量赋值
+ * @param path 相对于当前脚本this.node的搜索路径,当rootPath非空，则以rootPath为根节点查找
  * @param type 查找组件类型
+ * @param rootPath 相对于this.node 的搜索路径，不传入时，以当的this.node为根节点进行查找
  * @returns 
  */
-export function inject<T extends Component | Node>(path: string, type: FIND_TYPE<T>) {
+export function inject<T extends Component | Node>(path: string, type: FIND_TYPE<T>, rootPath?: string) {
     return function (target: any, member: string) {
         if (!(target instanceof Component)) {
             Log.e("无法注入,仅支持 Component 组件")
@@ -66,8 +69,20 @@ export function inject<T extends Component | Node>(path: string, type: FIND_TYPE
                             enumerable: true,
                             configurable: true,
                             get() {
+                                let node = self.node;
+                                if (ele.root) {
+                                    let rootMemberName = `__${ele.root.replace(/\//g, "_")}`
+                                    if (!isValid(self[rootMemberName])) {
+                                        self[rootMemberName] = __find(ele.root, node, Node);
+                                    }
+                                    node = self[rootMemberName];
+                                    if (DEBUG && !isValid(node)) {
+                                        Log.e(`${js.getClassName(self)}.${ele.root}节点不存在!!!`)
+                                    }
+                                }
+
                                 if (!isValid(self[key])) {
-                                    self[key] = __find(ele.path, self.node, ele.type);
+                                    self[key] = __find(ele.path, node, ele.type);
                                 }
                                 return self[key];
                             },
@@ -82,7 +97,7 @@ export function inject<T extends Component | Node>(path: string, type: FIND_TYPE
             Reflect.defineProperty(target, _FIND_OPTIONS_, { value: {} })
         }
 
-        let option: FindOption<T> = { path: path, type: type, member: member }
+        let option: FindOption<T> = { path: path, type: type, member: member, root: rootPath }
         let attribute = `__${member}`
         let fOption = Reflect.get(target, _FIND_OPTIONS_)
         Reflect.defineProperty(fOption, attribute, { value: option, enumerable: true })
