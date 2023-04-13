c4c41df47a4c986024de33b353e7fd80ce075ca8
zhengfasheng <zheng_fasheng@outlook.com>
2023-04-13 16:01:17 +0800

    inject 接口优化,再首次使用时，才进行节点查找

--- proj/assets/scripts/login/view/LoginView.ts
+++ proj/assets/scripts/login/view/LoginView.ts
@@ -1,4 +1,5 @@
 import GameView from "../../framework/core/ui/GameView";
+import { inject } from "../../framework/defines/Decorators";
 import { Macro } from "../../framework/defines/Macros";
 
 const { ccclass, property } = cc._decorator;
@@ -10,20 +11,16 @@ export default class LoginView extends GameView {
         return `@LoginView`;
     }
 
-    private _login: cc.Node = null;
+    @inject("login",cc.Node)
+    private login: cc.Node = null!;
+    @inject("version",cc.Label)
+    private version : cc.Label = null!;
 
     onLoad() {
         super.onLoad();
-        this._login = cc.find("login", this.node);
-        let version = cc.find("version",this.node)?.getComponent(cc.Label);
-        if ( version ){
-            version.string = App.updateManager.getVersion(this.bundle);
-        }
-        this._login.on(cc.Node.EventType.TOUCH_END, () => {
+        this.version.string = App.updateManager.getVersion(this.bundle);
+        this.login.on(cc.Node.EventType.TOUCH_END, () => {
             this.enterBundle(Macro.BUNDLE_HALL);
         });
-
-        // let test = cc.find("test",this.node).getComponent(cc.Label)
-        // test.string = Manager.utils.convertValue(-1123456222.8844,4)
     }
 }
