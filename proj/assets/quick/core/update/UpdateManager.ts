d25d0bcd878cc233224e39645e728f8fec9014ea
zhengfasheng <zheng_fasheng@outlook.com>
2024-12-09 13:34:41 +0800

    注释修改

--- proj/assets/quick/core/update/UpdateManager.ts
+++ proj/assets/quick/core/update/UpdateManager.ts
@@ -401,7 +401,7 @@ export class UpdateManager implements ISingleton {
     }
 
     debug() {
-        Log.d(`-----------热火更新管理器中相关信息------------`);
+        Log.d(`-----------热更新管理器中相关信息------------`);
         Log.dump({ name: "远程版本信息", data: this.remoteVersions });
     }
 }
