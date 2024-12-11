9e8d85bcc6edf810a5c65f02c887b26049e1480b
zhengfasheng <547933762@qq.com>
2024-12-11 23:47:04 +0800

    1，音频组件优化 2，uiManager优化

--- proj/assets/quick/core/ui/UISprite.ts
+++ proj/assets/quick/core/ui/UISprite.ts
@@ -195,17 +195,20 @@ export default class UISprite extends cc.Sprite {
             let loaded = App.bundleManager.getBundle(realBundle);
             if (!loaded) {
                 // Log.d(`${realBundle}未加载`);
+                if ( CC_EDITOR ){
+                    this.spriteFrame = null!;
+                }
                 return;
             }
 
             let url = App.getLanguage(this.language as any, this.params, realBundle)
             if (!url) {
+                if ( CC_EDITOR ){
+                    this.spriteFrame = null!;
+                }
                 return;
             }
             let view = await App.uiManager.getViewAsync(this.user);
-            if (!view) {
-                return;
-            }
             if (this.isRemote) {
                 // Log.d("加载远程图片")
                 this.loadRemoteImage({
