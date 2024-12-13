808f0d411a7c6929a833e02eb75dfb73b3ec30c7
zhengfasheng <547933762@qq.com>
2024-12-13 22:08:50 +0800

    debugView界面优化

--- proj/assets/scripts/debug/DebugView.ts
+++ proj/assets/scripts/debug/DebugView.ts
@@ -7,6 +7,11 @@ import { Macro } from '../../quick/defines/Macros';
 import UIView from '../../quick/core/ui/UIView';
 const { ccclass, property } = cc._decorator;
 
+interface Data {
+    text: string;
+    onEvent: () => void;
+}
+
 @ccclass
 export class DebugView extends UIView {
 
@@ -14,75 +19,141 @@ export class DebugView extends UIView {
         return "common/prefabs/DebugView";
     }
 
-    @inject("logView",cc.Node)
+    @inject("logView", cc.Node)
     private logView: cc.Node = null!;
-    @inject("content",cc.Node)
+    @inject("content", cc.Node)
     private content: cc.Node = null!;
-    @inject("background",cc.Node)
-    private background : cc.Node = null!;
-    @inject("background",cc.Node,"logView")
-    private logViewBackground : cc.Node = null!;
+    @inject("item", cc.Node,"content")
+    private itemPrefab: cc.Node = null!;
+    @inject("background", cc.Node)
+    private background: cc.Node = null!;
+    @inject("background", cc.Node, "logView")
+    private logViewBackground: cc.Node = null!;
+
+    get config() {
+        let config: Data[] = [
+            {
+                text: "显示视图",
+                onEvent: this.onShowUI,
+            },
+            {
+                text: "显示节点",
+                onEvent: this.onShowNode,
+            },
+            {
+                text: "资源缓存",
+                onEvent: this.onShowRes,
+            },
+            {
+                text: "显示组件",
+                onEvent: this.onShowComp,
+            },
+            {
+                text: "调试信息",
+                onEvent: this.onShowDebugInfo,
+            },
+            {
+                text: "日志",
+                onEvent: this.onLog,
+            },
+            {
+                text: "逻辑管理器",
+                onEvent: this.onLogicManager,
+            },
+            {
+                text: "数据中心",
+                onEvent: this.onDataCenter,
+            },
+            {
+                text: "Bundle入口",
+                onEvent: this.onEntry,
+            },
+            {
+                text: "Proto信息",
+                onEvent: this.onProto,
+            },
+            {
+                text: "Bundle管理器",
+                onEvent: this.onBundleMgr,
+            },
+            {
+                text: "节点缓存池",
+                onEvent: this.onPool,
+            },
+            {
+                text: "Handlers",
+                onEvent: this.onHandler,
+            },
+            {
+                text: "Sandlers",
+                onEvent: this.onSender,
+            },
+            {
+                text: "网络管理器",
+                onEvent: this.onServiceManager,
+            },
+            {
+                text: "热更新",
+                onEvent: this.onHotUpdate,
+            },
+            {
+                text: "内存警告模拟",
+                onEvent: this.onLowMemory,
+            },
+            {
+                text: "释放管理器",
+                onEvent: this.onReleaseManager,
+            },
+            {
+                text: "适配器",
+                onEvent: this.onAdaptor,
+            },
+            {
+                text: "当前单例",
+                onEvent: this.onSingleton,
+            }
+        ]
+
+        return config;
+    }
+
+    private initData(){
+        const config = this.config;
+        this.content.removeAllChildren();
+        config.forEach(v=>{
+            const node = cc.instantiate(this.itemPrefab);
+            node.name = v.text;
+            cc.find("Label",node).getComponent(cc.Label).string = v.text;
+            this.onN(node,cc.Node.EventType.TOUCH_END,v.onEvent);
+            this.content.addChild(node);
+        })
+    }
+
     onLoad() {
         super.onLoad();
-        //显示界面信息
-        this.bindEvent("showUI",this.onShowUI);
-        //显示节点信息
-        this.bindEvent("showNode",this.onShowNode);
-        //显示资源缓存信息
-        this.bindEvent("showRes",this.onShowRes);
-        //显示当前组件信息
-        this.bindEvent("showComponent",this.onShowComp);
-        //显示调试信息
-        this.bindEvent("showDebugInfo",this.onShowDebugInfo);
-        this.bindEvent("log",this.onLog);
-        //逻辑管理器信息输出
-        this.bindEvent("logic",this.onLogicManager);
-        //数据中心
-        this.bindEvent("dataCenter",this.onDataCenter);
-        //bundle入口管理器
-        this.bindEvent("entry",this.onEntry);
-        //proto 信息输出 
-        this.bindEvent("proto",this.onProto);
-        //bundle管理器
-        this.bindEvent("bundleMgr",this.onBundleMgr);
-        //节点缓存池
-        this.bindEvent("pool",this.onPool);
-        //Senders
-        this.bindEvent("sender", this.onSender);
-        this.bindEvent("handler", this.onHandler);
-        //网络管理器
-        this.bindEvent("serviceManager",this.onServiceManager);
-        //热火更新管理
-        this.bindEvent("hotupdate",this.onHotUpdate);
-        //内存警告
-        this.bindEvent("lowMemory",this.onLowMemory);
-        //释放管理器
-        this.bindEvent("releaseManager",this.onReleaseManager);
-        //适配器
-        this.bindEvent("adaptor", this.onAdaptor);
-        //当前所有单例
-        this.bindEvent("singleton", this.onSingleton);
+        this.itemPrefab.removeFromParent();
+        this.initData();
         this.doOther();
     }
 
-    private doOther(){
+    private doOther() {
         if (this.logView) {
             this.logView.active = false;
             this.initLogView();
         }
-        this.onN(this.background,cc.Node.EventType.TOUCH_END, () => {
-            if ( this.args.onClose ) this.args.onClose();
+        this.onN(this.background, cc.Node.EventType.TOUCH_END, () => {
+            if (this.args.onClose) this.args.onClose();
             this.close();
         });
     }
 
-    private bindEvent(path : string ,cb:()=>void){
-        let node = cc.find(path,this.content);
-        this.onN(node,cc.Node.EventType.TOUCH_END,cb,this);
+    private bindEvent(path: string, cb: () => void) {
+        let node = cc.find(path, this.content);
+        this.onN(node, cc.Node.EventType.TOUCH_END, cb, this);
     }
 
     private initLogView() {
-        this.onN(this.logViewBackground,cc.Node.EventType.TOUCH_END, () => {
+        this.onN(this.logViewBackground, cc.Node.EventType.TOUCH_END, () => {
             this.logView.active = false;
         });
 
@@ -95,7 +166,7 @@ export class DebugView extends UIView {
                     if (toggle) {
                         toggle.isChecked = App.logger.isValid(this.getLogLevel(i));
                     }
-                    this.onN(node,"toggle", (toggle: cc.Toggle) => {
+                    this.onN(node, "toggle", (toggle: cc.Toggle) => {
                         if (toggle.isChecked) {
                             App.logger.attach(this.getLogLevel(i));
                         } else {
@@ -137,40 +208,40 @@ export class DebugView extends UIView {
         App.bundleManager.debug();
     }
 
-    private onPool(){
+    private onPool() {
         App.pool.debug();
     }
 
-    private onLog(){
+    private onLog() {
         this.logView.active = true;
     }
 
-    private onShowDebugInfo(){
+    private onShowDebugInfo() {
         cc.debug.setDisplayStats(!cc.debug.isDisplayStats())
-        App.storage.setItem(Macro.SHOW_DEBUG_INFO_KEY,cc.debug.isDisplayStats());
+        App.storage.setItem(Macro.SHOW_DEBUG_INFO_KEY, cc.debug.isDisplayStats());
     }
 
-    private onShowUI(){
-        App.uiManager.debug({showViews:true});
+    private onShowUI() {
+        App.uiManager.debug({ showViews: true });
     }
 
-    private onShowNode(){
-        App.uiManager.debug({showChildren:true});
+    private onShowNode() {
+        App.uiManager.debug({ showChildren: true });
     }
 
-    private onShowRes(){
+    private onShowRes() {
         App.cache.debug();
     }
 
     private onShowComp() {
-        App.uiManager.debug({showComp:true});
+        App.uiManager.debug({ showComp: true });
     }
 
     private onSender() {
         App.senderManager.debug();
     }
 
-    private onHandler(){
+    private onHandler() {
         App.handlerManager.debug();
     }
 
@@ -182,7 +253,7 @@ export class DebugView extends UIView {
         App.updateManager.debug()
     }
 
-    private onLowMemory(){
+    private onLowMemory() {
         App.onLowMemory();
     }
 
@@ -198,8 +269,8 @@ export class DebugView extends UIView {
         Log.d(`设备或浏览器像素比例: ${cc.view.getDevicePixelRatio()}`);
         Log.d(`返回视图窗口可见区域像素尺寸: ${cc.view.getVisibleSizeInPixel().width} x ${cc.view.getVisibleSizeInPixel().height}`);
         Log.d(`当前场景设计分辨率: ${cc.view.getDesignResolutionSize().width} x ${cc.view.getDesignResolutionSize().height}`);
-        let viewRate = cc.view.getFrameSize().width/cc.view.getFrameSize().height;
-        let designRate = cc.view.getDesignResolutionSize().width/cc.view.getDesignResolutionSize().height;
+        let viewRate = cc.view.getFrameSize().width / cc.view.getFrameSize().height;
+        let designRate = cc.view.getDesignResolutionSize().width / cc.view.getDesignResolutionSize().height;
         Log.d(`视图宽高比:${viewRate}`);
         Log.d(`设置分辨率宽高比:${designRate}`);
     }
