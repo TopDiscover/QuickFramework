13399dc02f5bdaa0def5be7657d1689e94b6be9c
zhengfasheng <zheng_fasheng@outlook.com>
2025-01-15 11:01:28 +0800

    工具优化

--- tools/src/core/FileUtils.ts
+++ tools/src/core/FileUtils.ts
@@ -24,12 +24,8 @@ export default class FileUtils extends Handler {
      */
     async symlinkSync(target: PathLike, path: PathLike, type?: symlink.Type | null): Promise<void> {
         if (existsSync(path)) {
-            let stat = statSync(path);
-            if (stat.isDirectory()) {
-                // console.log(`删除目录:${path}`);
-                await this.delDir(path)
-            } else {
-                // console.log(`删除文件:${path}`);
+            const stat = statSync(path);
+            if ( stat.isSymbolicLink() ){
                 unlinkSync(path);
             }
         }
