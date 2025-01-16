915225611a0f146ed4e3f3350b25c03e18ec84fc
zhengfasheng <zheng_fasheng@outlook.com>
2025-01-16 16:50:47 +0800

    视图管理器优化

--- proj/assets/quick/core/asset/ViewAsset.ts
+++ proj/assets/quick/core/asset/ViewAsset.ts
@@ -117,8 +117,6 @@ export namespace ViewAsset {
         view: UIView = null!;
         /**@description 等待加载完成回调 */
         finishCb: ((view: any) => void)[] = [];
-        /**@description 等待获取界面回调 */
-        getViewCb: ((view: any) => void)[] = [];
         /**是否预加载,不显示出来，但会加到当前场景上 */
         isPreload: boolean = false;
         /**@description 是否通过预置创建 */
@@ -138,19 +136,7 @@ export namespace ViewAsset {
             return this.loadData.name!;
         }
 
-        private doGet(view: UIView | null, className: string, msg: string) {
-            for (let i = 0; i < this.getViewCb.length; i++) {
-                let cb = this.getViewCb[i];
-                if (cb) {
-                    cb(view);
-                    if (CC_DEBUG) Log.w(`ViewData do get view : ${className} msg : ${msg}`);
-                }
-            }
-
-            this.getViewCb = [];
-        }
-
-        private doFinish(view: UIView | null, className: string, msg: string) {
+        doFinish(view: UIView | null, className: string, msg: string) {
             for (let i = 0; i < this.finishCb.length; i++) {
                 let cb = this.finishCb[i];
                 if (cb) {
@@ -159,11 +145,6 @@ export namespace ViewAsset {
                 }
             }
             this.finishCb = [];
-        }
-
-        doCallback(view: UIView | null, className: string, msg: string) {
-            this.doFinish(view, className, msg);
-            this.doGet(view, className, msg);
         }
 
         resumeRelease() {
