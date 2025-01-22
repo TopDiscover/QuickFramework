99b1e9d7adfa267aebf737e6c93914c66e6d3989
zhengfasheng <zheng_fasheng@outlook.com>
2025-01-22 13:51:37 +0800

    网络优化

--- proj/assets/quick/core/net/ws/WSProxy.ts
+++ proj/assets/quick/core/net/ws/WSProxy.ts
@@ -45,7 +45,7 @@ export class WSProxy {
      */
     connect(url: string, timeOut: number = 10000, protocols?: string | string[]) {
         return new Promise<boolean>((resolve, reject) => {
-            const protocol = url.indexOf("wss") == 0 ? "wss" : "ws";
+            const protocol = url.startsWith("wss") ? "wss" : "ws";
             if (CC_JSB && protocol == "wss") {
                 if (!App.wssCacertUrl) {
                     Log.e(`${this.options.tag}请先设置wss的证书url,Launch脚本中直接挂载证书`);
@@ -68,7 +68,6 @@ export class WSProxy {
                     }
                 } catch (error) {
                     CC_DEBUG && Log.e(`${this.options.tag}WebSocket连接时发生错误:`, error);
-                } finally {
                     this.doOpen(false);
                 }
             }, timeOut);
@@ -137,13 +136,13 @@ export class WSProxy {
                 CC_DEBUG && Log.w(this.options.tag, `关闭时，网络未连接`);
                 return
             }
-            if ( this.status == WebSocket.CLOSED ){
+            if (this.status == WebSocket.CLOSED) {
                 resolve();
                 this.doClose();
                 CC_DEBUG && Log.w(this.options.tag, `关闭时，网络已经关闭`);
                 return
             }
-            if ( this.status != WebSocket.CLOSING ){
+            if (this.status != WebSocket.CLOSING) {
                 this.ws.close(code, reason);
             }
             this.rsClose = resolve;
@@ -161,23 +160,24 @@ export class WSProxy {
         })
     }
 
-    send(data: SocketBuffer):boolean {
+    send(data: SocketBuffer): boolean {
         if (!this.ws || !data) {
+            CC_DEBUG && Log.w(this.options.tag, `发送消息失败: 无效的连接或数据`);
             return false;
         }
-        if (this.status === WebSocket.OPEN) {
-            this.ws.send(data);
-            return true;
-        } else {
-            if (this.status === WebSocket.CONNECTING) {
+        switch (this.status) {
+            case WebSocket.OPEN:
+                this.ws.send(data);
+                return true;
+            case WebSocket.CONNECTING:
                 this.waitSend.push(data);
                 return true;
-            } else {
-                //关闭或者正在关闭状态
-                let content = this.status == WebSocket.CLOSING ? `网络正在关闭` : `网络已经关闭`;
-                CC_DEBUG && Log.w(this.options.tag, `发送消息失败: ${content}`);
+            case WebSocket.CLOSING:
+            case WebSocket.CLOSED:
+                CC_DEBUG && Log.w(this.options.tag, `发送消息失败: ${this.status === WebSocket.CLOSING ? '网络正在关闭' : '网络已经关闭'}`);
                 return false;
-            }
+            default:
+                return false;
         }
     }
 }
